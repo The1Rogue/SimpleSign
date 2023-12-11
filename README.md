20685572584e1a6238a18c7b362265ac6c6c2488
The1Rogue <fabiantschut@gmail.com>
2023-12-11 11:58:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# SimpleSign
+SimpleSign is a low maintenance, easy to set up and use system for digital signage, utilizing the to everyone familiar format of powerpoint / presentations.
+
+### Setup
+
+##### credentials
+since this is an open source repo and i don't want to have to care to much about credentials and security, you will have to get your own api key,
+which can be done here: https://console.cloud.google.com/apis/
+
+place the credentials in the same folder as `main.py`, and name the file `creds.json`
+
+##### drive setup
+in a Google Drive account of your choosing, create a directory. any files in this directory will be used and displayed by SimpleSign.
+clone this repository onto the designated device which will display the signage.
+run `python main.py [folder]` where folder is the name of the folder you created. the first time this is run, a browser will prompt you to log in to link your Google account.
+after that the files will be downloaded, converted and subsequently displayed.
+it is recommended that this program is set to run automatically after booting.
+
+##### requirements
+to authenticate to google, a web-browser is required on the target system. this is not necessary if the display only is used, and files are manually placed into the buffer.
+
+`vlc` is required for displaying the result.
+
+for all systems the following python packages are needed:
+`pdf2image, google-api-python-client, google-auth-oauthlib`
+
+### Settings
+`-d / --delay` sets the time every slide is shown for, in seconds, defaults to 10s.
+
+`-b / --buffer` files fetched from the drive will be stored locally in the folder provided, defaults to buf/
+
+`--no-display` when present, will only download and update the buffer, without displaying its contents. which means no graphical environment needs to be present.
+
+`--no-update` when present, will not download any files, and only display what is present in the buffer. requires no authentication.
+
---
+++ main.py
@@ -0,0 +1,129 @@
+import os
+import io
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import pdf2image
+import argparse
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly'] # scope needed, do not change
+
+parser = argparse.ArgumentParser(
+                    prog='SimpleSign',
+                    description='Display sign details easily')
+
+parser.add_argument("folder", help="the name of the remote folder")
+parser.add_argument("-d", "--delay", default=10, type=int, help="the time each slide is shown for")
+parser.add_argument("-b", "--buffer", default="buf/", type=str, help="the local folder pngs are kept")
+parser.add_argument("--no-display", dest="noDisplay", action="store_true", help="only update the buffer, do not display")
+parser.add_argument("--no-update", dest="noUpdate", action="store_true", help="only display, do not update buffer")
+
+
+# get authentication details
+def auth():
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    #either no token exist, or it expired
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token: #we have a refresh token
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'creds.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+# get a folder id
+def getFolder(name, service):
+    try:
+        folderId = service.files().list(q=f"mimeType = 'application/vnd.google-apps.folder' and name = '{name}'",
+                                        pageSize=10,
+                                        fields="nextPageToken, files(id, name)").execute()
+        folder = folderId.get('files', [])[0].get('id')
+        return folder
+
+    except HttpError as error:
+        print(f'While searching for folder {name}, An error occurred: {error}')
+
+# get the file ids/names of all files in a folder
+def listFiles(folder, service, mimetype = None):
+    if mimetype is not None:
+        results = service.files().list(q=f"'{folder}' in parents and mimeType = '{mimetype}'", pageSize=10,
+                                       fields="nextPageToken, files(id, name)").execute()
+    else:
+        results = service.files().list(q = f"'{folder}' in parents", pageSize=10,
+                                       fields="nextPageToken, files(id, name, mimeType)").execute()
+    return results.get('files', [])
+
+
+# get a file id that can be exported
+def downloadFile(file_id, service, asPdf = True):
+    try:
+        if asPdf:
+            request = service.files().export_media(fileId=file_id, mimeType="application/pdf")
+        else:
+            request = service.files().get_media(fileId=file_id)
+
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        print()
+        while not done:
+            status, done = downloader.next_chunk()
+            print(f'Downloading {file_id} {int(status.progress() * 100)}%', end = "\r")
+
+        return file.getvalue()
+
+    except HttpError as error:
+        print(f'While exporting file with id: {file_id}, An error occurred: {error}')
+
+# get a file id as a list of images
+def getIMG(file_id, service):
+    pdfData = downloadFile(file_id, service)
+    return pdf2image.convert_from_bytes(pdfData)
+
+# renew the buffer folder with files from remote
+def updateSlides(folder, buffer):
+    for i in os.listdir(buffer):
+        os.remove(f"{buffer}/{i}")
+
+    # authenticate
+    creds = auth()
+    service = build('drive', 'v3', credentials=creds)
+    # fetch ids
+    folder = getFolder(folder, service)
+    files = listFiles(folder, service)
+    # download files
+    for i in files:
+        if i["mimeType"] == "video/mp4":
+            with open(f"{buffer}/{i['name']}", "wb") as file:
+                file.write(downloadFile(i["id"], service, False))
+        else:
+            data = getIMG(i["id"], service)
+            for j in range(len(data)):
+                data[j].save(f"{buffer}/{i['name']}-{j}.png")
+
+# display the buffer as a slideshow
+def display(delay, buffer):
+    os.system(f"vlc {buffer}/* --no-video-title-show --fullscreen --loop --image-duration={delay}")
+
+def main():
+    args = parser.parse_args()
+    if not args.noUpdate:
+        updateSlides(args.folder, os.path.abspath(args.buffer))
+    if not args.noDisplay:
+        display(args.delay, os.path.abspath(args.buffer))
+
+main()
